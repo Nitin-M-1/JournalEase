c314ee4e4e1c718d2afe2f815412fa4ee8293d57
Nitin-M-1 <condyman1234@gmail.com>
2025-01-13 14:56:15 +0530

    data update

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,12 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import "react-quill-new/dist/quill.snow.css";
+import { Icon } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Reflct",
+  title: "JournalEase:",
   description: "",
 };
 
